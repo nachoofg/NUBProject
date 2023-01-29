8220812de9382babef36b8c11b759905a5a27266
nacho <76927919+tsuchipls@users.noreply.github.com>
2023-01-29 07:02:04 -0300

    Update putreg.c

--- void/putreg.c
+++ void/putreg.c
@@ -5,7 +5,7 @@ void putreg(char str[30], int value)
 {
     // a = append
     // Appends to a file. Writing operations, append data at the end of the file.
-    FILE *arch = fopen("bnkreg.txt", "a");
+    FILE *arch = fopen("movelog.txt", "a");
     fprintf(arch, "%s: %d\n", str, value);
     fclose(arch);
 }
